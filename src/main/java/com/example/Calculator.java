3fa7965fabbbc859d30c77af242bdace52fcf9a6
Giomit99 <81476436+Giomit99@users.noreply.github.com>
2023-11-19 18:22:10 +0100

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -3,9 +3,10 @@ package com.example;
 public class Calculator {
   public int evaluate(final String pExpression) {
     int sum = 0;
-    for (String summand : pExpression.split("\\+")) {
+    
+    for (String summand : pExpression.split("\\+")) 
       sum += Integer.parseInt(summand);
-    }
+    
     return sum;
   }
 
